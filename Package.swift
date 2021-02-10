d509b5f145a6515ca02221983ef5a46f012ab008
Augusto Reis <augusto.reis@cabal.com.br>
2021-02-09 22:31:39 -0300

    Update Package.swift file

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "InngageIos",
-    platforms: [.iOS(.v10)],
+    platforms: [.iOS(.v11)],
     products: [
         .library(
             name: "InngageIos",
@@ -17,9 +17,7 @@ let package = Package(
             name: "InngageIos",
             dependencies: [],
             path: "InngageIos",
-            cSettings: [
-                .headerSearchPath("Internal"),
-            ]),
+            publicHeadersPath: ""),
         .testTarget(
             name: "InngageIosTests",
             dependencies: ["InngageIos"]),
